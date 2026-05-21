a7caea14ce3b3c6c50b533dca3c02a11b6242f54
Bradapple <Bradley_932@hotmail.com>
2026-05-21 22:06:54 +0100

    Moved to new API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,33 @@
-const token = "b7b67f23aa9d47668593cf5535f5db24";
+const YOUR_KEY = "b2618e622a4349958c0d425840ef7cc5";
 
 document.getElementById('team-select').addEventListener('change', function (e) {
     const teamId = e.target.value;
-    console.log(teamId);
 
     if (teamId !== "") {
-        fetch(`https://api.football-data.org/v4/teams/${teamId}`, {
+        fetch(`https://v3.football.api-sports.io/teams?id=${teamId}`, {
             headers: {
-                'X-Auth-Token': token
+                "X-RapidAPI-Key": YOUR_KEY,
+                "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
             }
         })
+            .then(res => res.json())
+            .then(data => {
+                const team = data.response[0].team;
+                const venue = data.response[0].venue;
+                console.log(data);
+                
+                
+                document.getElementById('team-info').innerHTML = `
+                    <h2>${team.name}</h2>
+                    <img src="${team.logo}" width="100">
+                    <p><strong>Founded:</strong> ${team.founded}</p>
+                    <p><strong>Country:</strong> ${team.country}</p>
 
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            //document.getElementById('team-info').textContent = data.name;
-            console.log(data);
-        })
-        .catch(function(error) {
-            console.log('Error:', error);
-        });
+                    <h3>${venue.name}</h3>
+                    <img src="${venue.image}" width="200">
+                    <p><strong>City:</strong> ${venue.city}</p>
+                    <p><strong>Capacity:</strong> ${venue.capacity.toLocaleString()}</p>`
+            })
+            .catch(err => console.error(err));
     }
 });
