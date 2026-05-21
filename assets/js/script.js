19230d726263e208adf494309c76f47a206b1df9
Bradapple <Bradley_932@hotmail.com>
2026-05-21 21:34:31 +0100

    changes made

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const token = "b7b67f23aa9d47668593cf5535f5db24";
 
 document.getElementById('team-select').addEventListener('change', function (e) {
     const teamId = e.target.value;
+    console.log(teamId);
 
     if (teamId !== "") {
         fetch(`https://api.football-data.org/v4/teams/${teamId}`, {
